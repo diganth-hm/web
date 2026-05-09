1f405a04de3677a9e861e5a5910ae75776ee6a42
diganth-hm <diganthhm714@gmail.com>
2026-05-09 16:03:08 +0530

    small changes

--- library/frontend/src/main.jsx
+++ library/frontend/src/main.jsx
@@ -1,45 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
 import "./index.css";
-
-import Login from "./sections/login";
-
-function Dashboard() {
-  return (
-    <h1 className="text-4xl font-bold text-center mt-20">
-      Dashboard Page
-    </h1>
-  );
-}
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
-
   <StrictMode>
-
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route
-          path="/"
-          element={<Login />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
-
+    <App />
   </StrictMode>
 );
