e2ea0c5e2785ed4b380f989c642ca18b9e37ab97
diganth-hm <diganthhm714@gmail.com>
2026-05-12 19:02:00 +0530

    wrote sidebar component

---
+++ library/frontend/src/components/sidebar.jsx
@@ -0,0 +1,38 @@
+import { BookOpen, LogOut,LibraryBig,BookOpenText } from "lucide-react";
+function Sidebar()
+{
+    return(
+        <div className="bg-blue-800 h-screen w-1/5 rounded-lg   text-white">
+         <div className="flex  items-center gap-3 mb-10 ml-10">
+           <div className="flex mt-10 ml-6"><LibraryBig size={35}/>
+            <h1 className="text-3xl font-bold">LibraryMS</h1>
+            </div>
+            </div>   
+
+         <button className="w-full p-10">
+            <div className="flex items-center gap-3  hover:bg-blue-700 transition p-3 rounded-lg ">
+            <BookOpen size={30} className="ml-8"/>
+            <span className="text-3xl mb-1 ">Browse Books</span>
+         </div>
+         </button>
+         
+         <button className="w-full p-10">
+            <div className="flex items-center gap-3  hover:bg-blue-700 transition p-3 rounded-lg ">
+            <BookOpenText size={30} className="ml-8"/>
+            <span className="text-3xl mb-1 ">My Books</span>
+         </div>
+         </button>
+         
+         <button className="w-full p-10">
+            <div className="flex items-center gap-3  hover:bg-blue-700 transition p-3 rounded-lg ">
+            <LogOut size={30} className="ml-8"/>
+            <span className="text-3xl mb-1 ">Logout</span>
+         </div>
+         </button>
+ 
+
+        </div>
+    )
+
+}
+export default Sidebar;
