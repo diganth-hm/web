75015687675bf4b00460dbc833e6fc7f7321ac59
diganth-hm <diganthhm714@gmail.com>
2026-05-12 19:04:30 +0530

    started user dashboard

---
+++ library/frontend/src/sections/user/dash.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../components/sidebar";
+function User() {
+  return (
+    <div className="p-5 rounded-full" >
+      <Sidebar />
+    
+    </div>
+  );
+}
+
+export default User;
