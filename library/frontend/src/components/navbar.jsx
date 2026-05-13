e9ce6304700a44801008eb7b4c74e26d273f6dbe
diganth-hm <diganthhm714@gmail.com>
2026-05-13 18:03:59 +0530

    completed navbar sections

---
+++ library/frontend/src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import { Menu, Bell,UserPen } from "lucide-react";
+
+function Navbar() {
+  return (
+    <div className="w-full bg-white p-4 flex items-center justify-between">
+      
+
+      <div className="flex items-center">
+        
+        <button className="p-2 hover:bg-gray-200 transition rounded-lg">
+          <Menu size={32} />
+        </button>
+
+        <input
+          type="text"
+          placeholder="Search books by title, author or category..."
+          className="bg-gray-100 p-3 rounded-3xl w-[500px] outline-none ml-6"
+        />
+      </div>
+
+<div calssName="flex gap-5">
+      <button className="p-2 hover:bg-gray-200 rounded-full transition">
+        <Bell size={28} />
+      </button>
+     <button><UserPen size={10} className="ml-10  bg-blue-600 rounded-3xl h-10 w-10"/>
+     </button>
+</div>
+    </div>
+  );
+}
+
+export default Navbar;
