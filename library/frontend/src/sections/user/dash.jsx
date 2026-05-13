0a5a56236c42b88ba3d71002705f7414cd01b828
diganth-hm <diganthhm714@gmail.com>
2026-05-13 18:06:15 +0530

    added navbar component

--- library/frontend/src/sections/user/dash.jsx
+++ library/frontend/src/sections/user/dash.jsx
@@ -1,9 +1,12 @@
 import Sidebar from "../../components/sidebar";
+import Navbar from "../../components/navbar";
 function User() {
   return (
-    <div className="p-5 rounded-full" >
+    <div className="p-5 rounded-full flex h-screen" >
       <Sidebar />
-    
+      <div className="flex-1">  <Navbar/></div>
+   
+
     </div>
   );
 }
